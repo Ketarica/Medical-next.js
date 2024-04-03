d27411ac4af84afecbac42f3c7209d417fbdf933
Ketarica <120320477+Ketarica@users.noreply.github.com>
2024-04-03 10:28:55 +0300

    Update Buttons.tsx

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -4,22 +4,22 @@ import React, { useState } from "react";
 import ModalFirst from '@/components/ModalFirst'
 import ModalSecond from '@/components/ModalSecond'
 
-const YellowFinger = [
-    <Image 
-    src= "/assets/YellowFinger.png"
-    alt="YellowFinger"
-    width={48.26}
-    height={48.26}
-  />
-]
-const BlueFinger = [
-    <Image 
-    src= "/assets/BlueFinger.png"
-    alt="BlueFinger"
-    width={48.26}
-    height={48.26}
-  />
-]
+// const YellowFinger = [
+//     <Image 
+//     src= "/assets/YellowFinger.png"
+//     alt="YellowFinger"
+//     width={48.26}
+//     height={48.26}
+//   />
+// ]
+// const BlueFinger = [
+//     <Image 
+//     src= "/assets/BlueFinger.png"
+//     alt="BlueFinger"
+//     width={48.26}
+//     height={48.26}
+//   />
+// ]
 
   
 export default function Buttons(){
@@ -33,8 +33,18 @@ export default function Buttons(){
                 //  <div className="flex flex-col absolute w-full items-end gap-[186px] mt-[15px] pr-[20px] "> 
                 <div className="flex flex-col absolute w-full items-end gap-[570px] mt-[540px] pr-[20px] mobile-375:gap-[450px] mobile-375:mt-[500px] mobile-425:mt-[450px]
                 sm:gap-[260px] sm:mt-[30px] md:gap-[230px] lg:gap-[165px]" > 
-                  <button onClick={() => setShowMyModal(true)}> {BlueFinger} </button>
-                  <button className="sm:max-2xl:mt-[120px]" onClick={() => setShowMySecondModal(true)}>  {YellowFinger} </button>
+                  <button onClick={() => setShowMyModal(true)}>  <Image 
+    src= "/assets/BlueFinger.png"
+    alt="BlueFinger"
+    width={48.26}
+    height={48.26}
+  /></button>
+                  <button className="sm:max-2xl:mt-[120px]" onClick={() => setShowMySecondModal(true)}>    <Image 
+    src= "/assets/YellowFinger.png"
+    alt="YellowFinger"
+    width={48.26}
+    height={48.26}
+  /> </button>
                   <ModalFirst onClose={handleOnClose} visible = {ShowMyModal}/>
                   <ModalSecond onCloseSecond={handleOnCloseSecond} visibleSecond = {ShowMySecondModal}/>
                  </div>
